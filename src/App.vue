43210b8b5b4f9989b5af53a0e995f52091ea3da6
ChesterFL <Chester@favorlabs.io>
2022-09-15 23:04:58 +0800

    fix: get the endpoint followed by an extra /

--- src/App.vue
+++ src/App.vue
@@ -140,7 +140,7 @@ export default {
     },
     analyzingUrl() {
       const shareParams = location.hash.split('#/')[1];
-      const endPoint = new URLSearchParams(location.search).get("endpoint")
+      const endPoint = new URL(new URLSearchParams(location.search).get("endpoint")).origin
       this.$router.push({
         name: 'Config',
         params: {
