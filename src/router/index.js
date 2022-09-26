8d0f395bf39bb5d2691326caf199c6e9f86d5610
ChesterFL <Chester@favorlabs.io>
2022-09-26 18:35:37 +0800

    feat: add share

--- src/router/index.js
+++ src/router/index.js
@@ -6,180 +6,190 @@ import StudioNavBar from '@/components/StudioNavBar.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    components: {
-      NavBar,
-      default: () => import(/* webpackChunkName: "Home" */ '../views/Home.vue')
-    }
-  },
-  {
-    path: '/subscriptions',
-    name: 'Subscription',
-    components: {
-      NavBar,
-      default: () =>
-          import(/* webpackChunkName: "Subscription" */ '../views/Subscription.vue')
-    },
-    meta: {requiresAuth: true}
-  },
-  {
-    path: '/liked-videos',
-    name: 'LikedVideos',
-    components: {
-      NavBar,
-      default: () =>
-          import(/* webpackChunkName: "LikedVideos" */ '../views/LikedVideo.vue')
-    },
-    meta: {requiresAuth: true}
-  },
-  {
-    path: '/signin',
-    name: 'SignIn',
-    component: () =>
-        import(/* webpackChunkName: "SignIn" */ '../views/Auth/SignIn.vue'),
-    meta: {requiresVisitor: true}
-  },
-  {
-    path: '/signup',
-    name: 'SignUp',
-    component: () =>
-        import(/* webpackChunkName: "SignUp" */ '../views/Auth/SignUp.vue'),
-    meta: {requiresVisitor: true}
-  },
-  {
-    path: '/trending',
-    name: 'Trending',
-    components: {
-      NavBar,
-      default: () =>
-          import(/* webpackChunkName: "Trending" */ '../views/Trending.vue')
-    }
-  },
-  {
-    path: '/studio',
-    components: {
-      StudioNavBar,
-      default: () =>
-          import(/* webpackChunkName: "Studio" */ '../views/Studio/Index.vue')
-    },
-    children: [
-      {
+    {
         path: '/',
-        name: 'Dashboard',
-        component: () =>
-            import(
-                /* webpackChunkName: "Studio" */ '../views/Studio/Dashboard.vue'
-                )
-      },
-      {
-        path: 'videos',
-        name: 'Video',
-        component: () =>
-            import(/* webpackChunkName: "Studio" */ '../views/Studio/Video.vue')
-      },
-      {
-        path: 'details/:id',
-        name: 'Detail',
-        component: () =>
-            import(/* webpackChunkName: "Studio" */ '../views/Studio/Details.vue')
-      }
-    ],
-    meta: {requiresAuth: true}
-  },
-  {
-    path: '/channels/:id',
-    components: {
-      NavBar,
-      default: () =>
-          import(/* webpackChunkName: "Channels" */ '../views/Channel/Index.vue')
+        name: 'Home',
+        components: {
+            NavBar,
+            default: () => import(/* webpackChunkName: "Home" */ '../views/Home.vue')
+        }
     },
-    children: [
-      {
-        path: '/',
-        name: 'ChannelHome',
-        component: () =>
-            import(
-                /* webpackChunkName: "Channels" */ '../views/Channel/Home.vue'
-                )
-      }
-    ]
-  },
-  {
-    path: '/watch/:id',
-    name: 'Watch',
-    components: {
-      NavBar,
-      default: () =>
-          import(/* webpackChunkName: "Watch" */ '../views/Watch.vue')
-    }
-  },
-  {
-    path: '/history',
-    name: 'History',
-    components: {
-      NavBar,
-      default: () =>
-          import(/* webpackChunkName: "History" */ '../views/History.vue')
+    {
+        path: '/share',
+        name: 'Share',
+        components: {
+            NavBar,
+            default: () =>
+                import(/* webpackChunkName: "Subscription" */ '../views/ShareInfo.vue')
+        },
+        meta: {requiresAuth: true}
     },
-    meta: {requiresAuth: true}
-  },
-  {
-    path: '/search',
-    name: 'Search',
-    components: {
-      NavBar,
-      default: () =>
-          import(/* webpackChunkName: "Search" */ '../views/Search.vue')
+    {
+        path: '/subscriptions',
+        name: 'Subscription',
+        components: {
+            NavBar,
+            default: () =>
+                import(/* webpackChunkName: "Subscription" */ '../views/Subscription.vue')
+        },
+        meta: {requiresAuth: true}
+    },
+    {
+        path: '/liked-videos',
+        name: 'LikedVideos',
+        components: {
+            NavBar,
+            default: () =>
+                import(/* webpackChunkName: "LikedVideos" */ '../views/LikedVideo.vue')
+        },
+        meta: {requiresAuth: true}
+    },
+    {
+        path: '/signin',
+        name: 'SignIn',
+        component: () =>
+            import(/* webpackChunkName: "SignIn" */ '../views/Auth/SignIn.vue'),
+        meta: {requiresVisitor: true}
+    },
+    {
+        path: '/signup',
+        name: 'SignUp',
+        component: () =>
+            import(/* webpackChunkName: "SignUp" */ '../views/Auth/SignUp.vue'),
+        meta: {requiresVisitor: true}
+    },
+    {
+        path: '/trending',
+        name: 'Trending',
+        components: {
+            NavBar,
+            default: () =>
+                import(/* webpackChunkName: "Trending" */ '../views/Trending.vue')
+        }
+    },
+    {
+        path: '/studio',
+        components: {
+            StudioNavBar,
+            default: () =>
+                import(/* webpackChunkName: "Studio" */ '../views/Studio/Index.vue')
+        },
+        children: [
+            {
+                path: '/',
+                name: 'Dashboard',
+                component: () =>
+                    import(
+                        /* webpackChunkName: "Studio" */ '../views/Studio/Dashboard.vue'
+                        )
+            },
+            {
+                path: 'videos',
+                name: 'Video',
+                component: () =>
+                    import(/* webpackChunkName: "Studio" */ '../views/Studio/Video.vue')
+            },
+            {
+                path: 'details/:id',
+                name: 'Detail',
+                component: () =>
+                    import(/* webpackChunkName: "Studio" */ '../views/Studio/Details.vue')
+            }
+        ],
+        meta: {requiresAuth: true}
+    },
+    {
+        path: '/channels/:id',
+        components: {
+            NavBar,
+            default: () =>
+                import(/* webpackChunkName: "Channels" */ '../views/Channel/Index.vue')
+        },
+        children: [
+            {
+                path: '/',
+                name: 'ChannelHome',
+                component: () =>
+                    import(
+                        /* webpackChunkName: "Channels" */ '../views/Channel/Home.vue'
+                        )
+            }
+        ]
+    },
+    {
+        path: '/watch/:id',
+        name: 'Watch',
+        components: {
+            NavBar,
+            default: () =>
+                import(/* webpackChunkName: "Watch" */ '../views/Watch.vue')
+        }
+    },
+    {
+        path: '/history',
+        name: 'History',
+        components: {
+            NavBar,
+            default: () =>
+                import(/* webpackChunkName: "History" */ '../views/History.vue')
+        },
+        meta: {requiresAuth: true}
+    },
+    {
+        path: '/search',
+        name: 'Search',
+        components: {
+            NavBar,
+            default: () =>
+                import(/* webpackChunkName: "Search" */ '../views/Search.vue')
+        }
+    },
+    {
+        path: '/config',
+        name: 'Config',
+        components: {
+            default: () =>
+                import(/* webpackChunkName: "Config" */ '../views/ApiConfig')
+        }
+    },
+    {
+        path: '/refresh',
+        name: 'Refresh',
+        components: {
+            default: () =>
+                import(/* webpackChunkName: "Refresh" */ '../views/refresh')
+        }
     }
-  },
-  {
-    path: '/config',
-    name: 'Config',
-    components: {
-      default: () =>
-          import(/* webpackChunkName: "Config" */ '../views/ApiConfig')
-    }
-  },
-  {
-    path: '/refresh',
-    name: 'Refresh',
-    components: {
-      default: () =>
-          import(/* webpackChunkName: "Refresh" */ '../views/refresh')
-    }
-  }
 ]
 
 const router = new VueRouter({
-  mode: 'hash',
-  routes
+    mode: 'hash',
+    routes
 })
 
 function SignInTips() {
-  // router.app.$options.router.push('/signin');
-  router.app.$options.store.dispatch("showTips", {
-    type: "info", text: "Please sign in"
-  })
+    // router.app.$options.router.push('/signin');
+    router.app.$options.store.dispatch("showTips", {
+        type: "info", text: "Please sign in"
+    })
 }
 
 router.beforeEach((to, from, next) => {
-  const loggedIn = localStorage.getItem('user')
+    const loggedIn = localStorage.getItem('user')
 
-  if (to.matched.some((record) => record.meta.requiresAuth) && !loggedIn) {
-    if (to.path === '/studio') {
-      SignInTips();
+    if (to.matched.some((record) => record.meta.requiresAuth) && !loggedIn) {
+        if (to.path === '/studio') {
+            SignInTips();
+        }
+        next('/')
+    } else if (
+        to.matched.some((record) => record.meta.requiresVisitor) &&
+        loggedIn
+    ) {
+        next('/')
+    } else {
+        next()
     }
-    next('/')
-  } else if (
-      to.matched.some((record) => record.meta.requiresVisitor) &&
-      loggedIn
-  ) {
-    next('/')
-  } else {
-    next()
-  }
 })
 
 export default router
---
+++ src/views/ShareInfo.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="main">
+    <div class="share">
+      <v-card class="card">
+        <div class="share_info">
+          <div class="info_box">
+            <div class="key">TOTAL</div>
+            <div class="value">{{ currentUser.invitations }}</div>
+          </div>
+        </div>
+        <div class="code_info">
+          <div class="code">
+            <qrcode-vue
+                size="150"
+                :value="url"
+            >
+            </qrcode-vue>
+          </div>
+          <div style="font-size: 12px;text-align: center;margin-top: 3px">
+            <span style="color: #707070">Invitation code:</span><span
+              style="color: #404040;font-weight: 500">{{ currentUser.code }}</span>
+          </div>
+        </div>
+        <div class="d-flex justify-space-between">
+          <v-btn
+              depressed
+              class="btn"
+              style="margin-right: 20px"
+          >
+            <ShareNetwork
+                network="telegram"
+                :url="url"
+                title=""
+                style=" color: #5D718B;text-decoration: none"
+            >
+              Share
+            </ShareNetwork>
+          </v-btn>
+          <v-btn
+              depressed
+              class="btn"
+              v-clipboard:copy="currentUser.code"
+          >
+            Copy
+          </v-btn>
+        </div>
+      </v-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+import QrcodeVue from 'qrcode.vue'
+
+export default {
+  name: "ShareInfo",
+  data: function () {
+    return {}
+  },
+  computed: {
+    ...mapGetters(["currentUser"]),
+    url() {
+      return `https://share.favorlabs.io/share/signin?invitation=${this.currentUser.code}`
+    }
+  },
+  components: {
+    QrcodeVue
+  }
+}
+</script>
+
+<style scoped>
+
+.share {
+  min-width: 300px;
+  padding: 5px;
+  background-image: url('../assets/share_bg.png');
+}
+
+.card {
+  overflow: hidden;
+  padding: 20px;
+}
+
+.key {
+  font-size: 14px;
+  font-family: Roboto-Medium, Roboto, serif;
+  font-weight: 500;
+  color: #ADADAD;
+  /*line-height: 0;*/
+}
+
+.value {
+  font-size: 16px;
+  font-family: DIN-Medium, DIN, serif;
+  font-weight: 500;
+  color: #1F1F1F;
+  text-align: center;
+}
+
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.share_info {
+  border-radius: 0 !important;
+  padding: 16px 0;
+  border: 1px solid #707070;
+  display: flex;
+  justify-content: center;
+}
+
+.info_box {
+  padding: 0 16px;
+}
+
+.code_info {
+  margin: 20px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.btn {
+  flex: 1;
+  height: 32px !important;
+  background-image: url('../assets/btn_bg.png');
+  background-size: 100% 100%;
+  color: #5D718B;
+  letter-spacing: 0;
+  overflow: hidden;
+}
+</style>
